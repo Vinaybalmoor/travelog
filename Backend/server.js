4fe42767c565ff1f4ec3f6bc7f1e16404f384538
vinayb <vinaybalmoor@gmail.com>
2026-02-11 19:59:23 +0530

    Backend changes

--- Backend/server.js
+++ Backend/server.js
@@ -4,9 +4,11 @@ const port = 5000;
 app.use(express.json());
 const authRoutes = require("./routes/auth");
 const connectDB = require("./config/db");
+const tripsRoutes = require("./routes/trips");
 require('dotenv').config();
 connectDB();
 app.use("/api/auth",authRoutes);
+app.use("/api/trips",tripsRoutes);
 app.get('/',(req,res)=>{
   res.send("Welcome to Travelog Backend Server");
 });
